022df0ccabf6e86efdfb2330555d213e7b5413fa
fernando camacho <fercamflo@live.com.mx>
2025-07-28 12:17:15 -0600

    C:\Users\ferca\Documents\web\pp

---
+++ db.sql
@@ -0,0 +1,16 @@
+CREATE TABLE propiedades (
+    nombre TEXT PRIMARY KEY UNIQUE,
+    descripcion    TEXT,
+    precio INTEGER,
+    rec INTEGER,
+    b INTEGER,
+    bc INTEGER,
+    mb INTEGER,
+    est INTEGER,
+    ter INTEGER,
+    const INTEGER,
+    direccion TEXT,
+    ubi TEXT
+)
+
+INSERT 
