c47ecac3f81a38020df76966881e49eb589b98d3
fernando camacho <fercamflo@live.com.mx>
2025-07-31 00:49:19 -0600

    added Bosque Real + changed catalog btn

--- cat/scripts.js
+++ cat/scripts.js
@@ -40,6 +40,7 @@
     precio : "9 200 000",
     ubicacion : "Av. Punto Sur 6259, 45645 Los Gavilanes, Jal.",
     maps: "https://maps.app.goo.gl/c3a7h5ogVZ4PeaVRA",
+    mapsembed : "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1867.7722143446026!2d-103.44776312996251!3d20.565812797001993!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8428acab4041817b%3A0x141e5c2aa620d401!2sAv.%20Punto%20Sur%206259%2C%2045645%20Los%20Gavilanes%2C%20Jal.!5e0!3m2!1sen!2smx!4v1753907195696!5m2!1sen!2smx",
     desc : "Excelente casa nueva en venta en PUNTO SUR ELITE, con terminados de lujo, a un costado de la Plaza Punto Sur",
     
     bgimg : "./puntosur/bgimg.jpg",
@@ -61,11 +62,32 @@
       precio : "6 450 000",
       ubicacion : "C. Tormenta 847, Jardines del Bosque, 44520 Guadalajara, Jal.",
       maps : "https://maps.app.goo.gl/7SwifZtiHS3hSia89",
+      mapsembed : "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2717.0766586696827!2d-103.37920270018157!3d20.663994135581756!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8428ae0a7b739c8d%3A0xd00a8be39aabb3f2!2sTormenta%20847%2C%20Jardines%20del%20Bosque%2C%2044520%20Guadalajara%2C%20Jal.!5e0!3m2!1sen!2smx!4v1753906654060!5m2!1sen!2smx",
       desc : "Casa en venta en Jardines del bosque, cuenta con área de sala comedor, recibidor, cocina integral con cubierta de granito, jardín posterior, medio baño y cochera para 2 autos en planta baja y en planta alta cuenta con 3  recámaras, 3 baños completos y una terraza",
       bgimg : "./tormenta/bgimg.jpg",
       img1 : "./tormenta/img1.jpg",
       img2 : "./tormenta/img2.jpg",
       img3 : "./tormenta/img3.jpg"
+    };
+
+    const bosquereal = {
+      nombre : "Se vende casa en Fraccionamiento Bosque Real",
+      casadepa : "casa",
+      ventarenta : "venta",
+      ter : "136",
+      const : "190",
+      rec : "4",
+      bc : "3",
+      est : "2",
+      precio : "4 500 000",
+      ubicacion : "45645 Bosque Real Fraccionamiento, Jalisco",
+      desc : "Excelente casa en Bosque Real, con 4 recámaras, 3 baños completos y un medio baño, estacionamiento para 2 autos y patio privado. Ubicada en Fraccionamiento Bosque Real, con amenidades como gimnasio, alberca, áreas verdes, casa club y espacio de coworking. ",
+      maps : "https://maps.app.goo.gl/KYyzork62tQYNJUGA",
+      mapsembed : "https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d643.6827786690162!2d-103.4944957020257!3d20.5692933839242!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2smx!4v1753907094429!5m2!1sen!2smx",
+      bgimg : "./bosquereal/bgimg.jpg",
+      img1 : "./bosquereal/img1.jpg",
+      img2 : "./bosquereal/img2.jpg",
+      img3 : "./bosquereal/img3.jpg"
     };
 
 
